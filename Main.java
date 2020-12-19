1ef1bfe2712f6f2568a1092dd248c12ec30ed9aa
Aftiah <76022936+Aftiah@users.noreply.github.com>
2020-12-19 16:06:59 +0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,29 @@
+package main;
+import model.AnggotaPerpus;
+public class Main {
+    
+    public static void main(String[] args){
+        AnggotaPerpus a= new AnggotaPerpus("5678","aaa","martapura",83158);
+        a.update();
+//        a.create();
+//        a.read();
+//        a.setId("1234");
+//        a.delete();
+//        a.delete("1234");
+
+//        a.setNama("Aftiah");
+//        a.setAlamat("Banjarbaru");
+//        a.setId(12345);
+//        a.setTelp(62831598);
+//        
+//        System.out.println(a.getNama());
+//        System.out.println(a.getId());
+//        System.out.println(a.getTelp());
+//        System.out.println(a.getAlamat());
+    }
+    
+            
+            
+}
+
+         
